e7dbd784d9a7c7c726d7e164bda9b5f5150c4658
Osqledarenmaster <40831701+Osqledarenmaster@users.noreply.github.com>
2022-12-01 13:51:27 +0100

    Update header.tsx

--- src/molecules/header.tsx
+++ src/molecules/header.tsx
@@ -6,7 +6,7 @@ import { Link } from 'gatsby';
 const Header: FunctionComponent = () => {
   return (
     <HeaderWrapper to="/">
-      <HeaderImage src={logo} />
+      <HeaderImage src={logo} /><br><br>
       <HeaderImage src={logo} />
     </HeaderWrapper>
   );
